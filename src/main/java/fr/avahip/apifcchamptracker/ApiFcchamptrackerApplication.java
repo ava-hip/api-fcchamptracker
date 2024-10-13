46968866a77a326b06ec4a901e55b53606829cdc
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-10-13 18:35:13 +0200

    Initial commit

---
+++ src/main/java/fr/avahip/apifcchamptracker/ApiFcchamptrackerApplication.java
@@ -0,0 +1,13 @@
+package fr.avahip.apifcchamptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFcchamptrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFcchamptrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/avahip/apifcchamptracker/ApiFcchamptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.avahip.apifcchamptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFcchamptrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
