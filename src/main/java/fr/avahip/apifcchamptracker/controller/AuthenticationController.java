7fbcd2043ded7ba5e83ca22bffbcbb2d26965acd
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-11-12 00:49:35 +0100

    feat: authentication improvements

--- src/main/java/fr/avahip/apifcchamptracker/controller/AuthenticationController.java
+++ src/main/java/fr/avahip/apifcchamptracker/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package fr.avahip.apifcchamptracker.controller;
 
 import fr.avahip.apifcchamptracker.service.AuthenticationService;
+import fr.avahip.apifcchamptracker.tools.AuthenticationRequest;
+import fr.avahip.apifcchamptracker.tools.AuthenticationResponse;
 import fr.avahip.apifcchamptracker.tools.RegistrationRequest;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +22,15 @@ public class AuthenticationController {
     public ResponseEntity<String> register(@Validated @RequestBody RegistrationRequest request) throws MessagingException {
         service.register(request);
         return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(@Validated @RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/activate-account")
+    public void confirm(@RequestParam String token) throws MessagingException {
+        service.activateAccount(token);
     }
 }
--- src/main/java/fr/avahip/apifcchamptracker/service/AuthenticationService.java
+++ src/main/java/fr/avahip/apifcchamptracker/service/AuthenticationService.java
@@ -1,13 +1,23 @@
 package fr.avahip.apifcchamptracker.service;
 
 import fr.avahip.apifcchamptracker.entity.User;
+import fr.avahip.apifcchamptracker.tools.AuthenticationRequest;
+import fr.avahip.apifcchamptracker.tools.AuthenticationResponse;
 import fr.avahip.apifcchamptracker.tools.RegistrationRequest;
 import jakarta.mail.MessagingException;
 
 public interface AuthenticationService {
+
     void register(RegistrationRequest request) throws MessagingException;
+
     void sendValidationEmail(User user) throws MessagingException;
+
     String generateAndSaveActivationToken(User user);
 
     String generateActivationCode(int length);
+
+    AuthenticationResponse authenticate(AuthenticationRequest request);
+
+    void activateAccount(String token) throws MessagingException;
 }
+
